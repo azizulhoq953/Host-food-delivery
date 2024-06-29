442c112e2bb224bc76c383831373573eb9260c51
azizulhoq953 <azizulhoq2041@gmail.com>
2024-06-29 21:44:02 +0600

    somechanges

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -1,66 +1,64 @@
-// import clientPromise from "../../../../libs/mongoConnect";
-// import {UserInfo} from "../../../../models/UserInfo";
-// import bcrypt from "bcrypt";
-// import * as mongoose from "mongoose";
-// import {User} from '../../../../models/User';
-// import NextAuth, {getServerSession} from "next-auth";
-// import CredentialsProvider from "next-auth/providers/credentials";
-// import GoogleProvider from "next-auth/providers/google";
-// import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "../../../../libs/mongoConnect";
+import {UserInfo} from "../../../../models/UserInfo";
+import bcrypt from "bcrypt";
+import * as mongoose from "mongoose";
+import {User} from '../../../../models/User';
+import NextAuth, {getServerSession} from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
-// export const authOptions = {
-//   secret: process.env.SECRET,
-//   adapter: MongoDBAdapter(clientPromise),
-//   providers: [
-//     GoogleProvider({
-//       clientId: process.env.GOOGLE_CLIENT_ID,
-//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-//     }),
-//     CredentialsProvider({
-//       name: 'Credentials',
-//       id: 'credentials',
-//       credentials: {
-//         username: { label: "Email", type: "email", placeholder: "test@example.com" },
-//         password: { label: "Password", type: "password" },
-//       },
-//       async authorize(credentials, req) {
-//         const email = credentials?.email;
-//         const password = credentials?.password;
+export const authOptions = {
+  secret: process.env.SECRET,
+  adapter: MongoDBAdapter(clientPromise),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      id: 'credentials',
+      credentials: {
+        username: { label: "Email", type: "email", placeholder: "test@example.com" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const email = credentials?.email;
+        const password = credentials?.password;
 
-//         mongoose.connect(process.env.MONGO_URL);
-//         const user = await User.findOne({email});
-//         const passwordOk = user && bcrypt.compareSync(password, user.password);
+        mongoose.connect(process.env.MONGO_URL);
+        const user = await User.findOne({email});
+        const passwordOk = user && bcrypt.compareSync(password, user.password);
 
-//         if (passwordOk) {
-//           return user;
-//         }
+        if (passwordOk) {
+          return user;
+        }
 
-//         return null
-//       }
-//     })
-//   ],
-// };
+        return null
+      }
+    })
+  ],
+};
 
-// export async function isAdmin() {
-//   const session = await getServerSession(authOptions);
-//   const userEmail = session?.user?.email;
-//   if (!userEmail) {
-//     return false;
-//   }
-//   const userInfo = await UserInfo.findOne({email:userEmail});
-//   if (!userInfo) {
-//     return false;
-//   }
-//   return userInfo.admin;
-// }
+export async function isAdmin() {
+  const session = await getServerSession(authOptions);
+  const userEmail = session?.user?.email;
+  if (!userEmail) {
+    return false;
+  }
+  const userInfo = await UserInfo.findOne({email:userEmail});
+  if (!userInfo) {
+    return false;
+  }
+  return userInfo.admin;
+}
 
-// const handler = NextAuth(authOptions);
+const handler = NextAuth(authOptions);
 
-// export { handler as GET, handler as POST }
+export { handler as GET, handler as POST }
 
 
-
-// new 
 // src/app/api/auth/[...nextauth]/route.js
 
 // import NextAuth from "next-auth/react";
@@ -119,29 +117,3 @@
 //   // Handle authorized POST request logic
 //   res.status(200).json({ message: "Authorized POST request" });
 // }
-// src/app/api/auth/[...nextauth]/route.js
-import { authOptions } from "../../../../config/auth";
-import { getServerSession } from "next-auth/react";
-import { UserInfo } from "../../../../models/UserInfo";
-
-export async function isAdmin(req) {
-  const session = await getServerSession({ req });
-  const userEmail = session?.user?.email;
-
-  if (!userEmail) {
-    return false;
-  }
-
-  const userInfo = await UserInfo.findOne({ email: userEmail });
-
-  if (!userInfo) {
-    return false;
-  }
-
-  return userInfo.admin;
-}
-
-// Export default NextAuth handler
-export default function handler(req, res) {
-  // Your NextAuth configuration and logic
-}
